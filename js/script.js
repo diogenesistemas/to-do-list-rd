14bee157cf98ee625342769da4555d2e149c20e4
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-05 17:42:04 -0300

    Adicionando Tarefas

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let btnAddTarefa = document.querySelector('#adicionar-tarefa');
+let tarefas = document.querySelector('#tarefas');
+let inputTarefa = document.querySelector('#tarefa-digitada');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btnAddTarefa.onclick = function () {
+//    tarefas.innerHTML += tarefaNova;
+// }
+
+
+
+btnAddTarefa.addEventListener('click', () => {
+    let valorDigitado = inputTarefa.value;
+
+    let tarefaNova = ` <div class="col-md-4">
+<div class="card-tarefa">
+    <div class="texto-tarefa">
+        ${valorDigitado}
+    </div>
+    <div class="botao-tarefa">
+        <img src="img/check.png" width="32" alt="Botão para finalizar tarefa"
+            title="Botão para finalizar tarefa">
+    </div>
+</div>
+</div>`
+
+    tarefas.innerHTML += tarefaNova
+
+});
